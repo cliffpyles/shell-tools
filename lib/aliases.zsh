9184fd3f8aec289a08a5ef55a49f4897a69b7ba5
Cliff Pyles <cliff@cliffpyles.com>
2022-10-16 06:35:50 -0400

    Add alias for creating github repos

--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -12,5 +12,6 @@ alias lsports="sudo lsof -PiTCP -sTCP:LISTEN"
 alias tf="terraform"
 
 # Version Control
+alias ighr="gh repo create $(basename $PWD) --private --source=. --remote=origin --push"
 alias clog="git log --graph --pretty=format:'%s' --abbrev-commit --no-merges"
 alias llog="git log --graph --pretty=format:'%Cred%h%Creset -%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --no-merges"
