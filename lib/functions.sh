4372f7bc59ccbab1cef2b58970c7fdd003af41f2
Cliff Pyles <cliff@cliffpyles.com>
2022-10-16 08:24:06 -0400

    Add about me

--- lib/functions.sh
+++ lib/functions.sh
@@ -12,3 +12,22 @@ function goto() {
   cd $(greadlink -f `which $1` | xargs dirname)
 }
 
+function aboutme() {
+  echo "  ┏━━ \033[1mAbout System\033[0m"
+  echo "  ┃"
+  echo "  ┣━━ \033[1mOS\033[0m"
+  echo "  ┃── Name: $(uname -s)"
+  echo "  ┃── Version: $(uname -r)"
+  echo "  ┃"
+  echo "  ┣━━ \033[1mProcessor\033[0m"
+  echo "  ┃── Architecture: $(uname -m)"
+  echo "  ┃── Type: $(uname -p)"
+  echo "  ┃"
+  echo "  ┣━━ \033[1mName\033[0m: $(uname -n)"
+  echo "  ┃"
+  echo "  ┣━━ \033[1mPublic IP\033[0m: $(curl --silent ifconfig.me)"
+  echo "  ┃"
+  echo "  ┣━━ \033[1mUser\033[0m: $(whoami)"
+  echo "  ┃"
+  echo "  ┗━━"
+}
