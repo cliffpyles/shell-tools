f2e91982080dac19d6cfe293c597787d9d8a584f
Cliff Pyles <cliff@cliffpyles.com>
2022-10-16 06:09:04 -0400

    Add functions and aliases

---
+++ lib/aliases.zsh
@@ -0,0 +1,18 @@
+#!/bin/zsh
+
+# Date/Time
+alias epoch="date -j -f '%a %b %d %T %Z %Y' '`date`' '+%s'"
+
+# File Management
+alias dup="cp -pR"
+alias fsize="du -csh ./*"
+
+# Networking
+alias lsports="sudo lsof -PiTCP -sTCP:LISTEN"
+
+# Tool Abbreviations
+alias tf="terraform"
+
+# Version Control
+alias clog="git log --graph --pretty=format:'%s' --abbrev-commit --no-merges"
+alias llog="git log --graph --pretty=format:'%Cred%h%Creset -%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --no-merges"
---
+++ lib/functions.zsh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+
+# File Management
+
+# Shows the resolved directory of an app in the path
+function show() {
+  greadlink -f `which $1` | xargs dirname
+}
+
+# Navigation
+
+# Sets the current working directory to the resolved directory of an app in the path
+function goto() {
+  cd $(greadlink -f `which $1` | xargs dirname)
+}
+
